b048e07d6efb70fcc9c4bb6fde0666463cd10247
GiGi Hurtault <gigi@hurtault.dev>
2025-08-05 22:48:50 -0400

    add chip module

---
+++ poker/lib/chip.ml
@@ -0,0 +1,2 @@
+(* not sure how i want to associate the values yet *)
+type t = White | Red | Green | Black | Purple
---
+++ poker/lib/player.ml
@@ -0,0 +1,6 @@
+open Card
+
+type hole_cards = Card.t * Card.t
+
+(* This should be something like a record or map i think, with the different chip types as keys *)
+type stack = int list
