e75f4bb54bd214061c2c92e54eebc832f9419170
GiGi Hurtault <gigi@hurtault.dev>
2025-08-05 22:57:44 -0400

    add more comments

--- poker/lib/dealer.ml
+++ poker/lib/dealer.ml
@@ -3,9 +3,13 @@ open Player
 
 type t
   
-let shuffle deck () = ();;
+let shuffle deck () =
+  (* TODO: implement shuffle algorithms. I kind of want to do a few. Also, should probably use Async *)
+  ();;
   
 module Deal = struct
+  (* Assign two hole cards per player *)
   let pre_flop deck players () = ();;
+  (* Add next card to the board after burning one card *)
   let next_card deck board () = ();;
 end
